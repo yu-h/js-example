6baecce426829da052a30a37791498da1bfc94bf
余昊 <894895714@qq.com>
2018-07-08 21:43:09 +0800

    scroll知识提交

--- src/dist/js/myFunc.js
+++ src/dist/js/myFunc.js
@@ -2,12 +2,12 @@
 /*
 用ID获取标签
  */
-function $(id){
+function _$(id){
 	return typeof id === "string" ? document.getElementById(id) : null;
 }
 
 /*
-获取屏幕可视区域的宽度
+获取屏幕可视区域的高度
  */
 function client(){
 	if (window.innerHeight !== undefined) {
@@ -68,4 +68,72 @@ function constant(obj,target,step){
 			obj.style.left = target + "px";
 		}
 	},20)
+}
+
+/*
+缓动动画函数
+缓动动画的基本原理：盒子本身的位置 + 步长（不断变化的，由大到小）
+步长：begin= begin + （end - begin）/ 缓动系数
+ */
+function buffer(obj,json,fn){
+	//清除定时器
+	clearInterval(obj.timer);
+	//设置定时器
+	var begin = 0, target = 0, speed = 0;
+	obj.timer = setInterval(function(){
+		//旗帜
+		var flag = true;
+		//json遍历
+		for(var key in json){
+			//获取初始值
+			if ("opacity" === key) {  //透明度
+				begin = Math.round(parseFloat(getCSSAttrValue(obj,key))*100) || 100;
+				target = parseInt(json[key]*100);
+			}else if ("scrollTop" === key) {
+				begin = Math.ceil(obj.scrollTop);
+				target = parseInt(json[key]);
+			}else{  //其他情况
+				begin = parseInt(getCSSAttrValue(obj,key)) || 0;
+				target = parseInt(json[key]);
+			}
+			//求出步长
+			speed = (target - begin)*0.2;
+			//判断是否向上取整
+			speed =(target > begin) ? Math.ceil(speed) : Math.floor(speed);
+			//动起来
+			if ("opacity" === key) {
+				obj.style.opacity = (begin + speed) / 100;
+				//ie
+				obj.style.filter = 'alpha(opacity:'+(begin + speed)+')'
+			}else if ("scrollTop" === key) {
+				obj.scrollTop = begin + speed;
+			}else{
+				obj.style[key] = begin + speed + "px";
+			}
+			//判断
+			if (begin !== target) {
+				flag = false;
+			}
+		};
+		//清除定时器
+		if (flag) {
+			clearInterval(obj.timer);
+
+			//判断有没有回调函数
+			if (fn) {
+				fn();
+			}
+		}
+	},20);
+}
+
+/*
+获取CSS样式值
+ */
+function getCSSAttrValue(obj,attr){
+	if (obj.currentStyle) {  //IE 和 opear
+		return obj.currentStyle[attr];
+	}else{
+		return window.getComputedStyle(obj, null)[attr];
+	}
 }
